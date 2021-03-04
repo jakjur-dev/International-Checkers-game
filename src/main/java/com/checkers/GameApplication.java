67421fd6e880be5577c73537a8a5d44bb6e8ac7d
jakjur-dev <jakub.jurczak46@gmail.com>
2021-03-04 17:34:17 +0100

    first commit

--- src/main/java/com/checkers/GameApplication.java
+++ src/main/java/com/checkers/GameApplication.java
@@ -1,9 +1,38 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.checkers;
 
-public class GameApplication {
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.scene.canvas.*;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
+public class GameApplication extends Application {
+
+    private static Stage primaryStage = new Stage();
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+
+        GridPane grid = new GridPane();
+        Scene scene = new Scene(grid, 585, 612, Color.BLACK);
+
+        primaryStage.setTitle("Checkers Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
+    public static void main (String[] args){
+        launch(args);
+    }
 }
---
+++ src/main/java/com/checkers/PiecePosition.java
@@ -0,0 +1,38 @@
+package com.checkers;
+
+public class PiecePosition {
+
+    private int column;
+    private int row;
+
+    public PiecePosition(int column, int row) {
+        this.column = column;
+        this.row = row;
+    }
+
+    public int getCol() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PiecePosition that = (PiecePosition) o;
+
+        if (column != that.column) return false;
+        return row == that.row;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = column;
+        result = 31 * result + row;
+        return result;
+    }
+}
